5645a888f84bf9dad769e340ce9d6249bbc86daa
guilhermeleroy <leroy.luiz@gmail.com>
2022-10-26 12:03:54 -0300

    Basic  logic in js done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//global variables
+let color = "black";
+
 function makeBoard(size) {
     let board = document.querySelector('.board');
 
+    //each time the board size is settled, we have to erase what we had and build the board from scratch 
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.remove());
 
@@ -9,19 +13,48 @@ function makeBoard(size) {
     
     for (let i = 0; i < size*size; i++) {
         let square = document.createElement('div');
-        //square.style.backgroundColor = 'blue';
+        square.style.backgroundColor = 'white';
         square.style.border = '0.2px solid lightgrey';
         board.insertAdjacentElement('beforeend', square);
+
+        square.addEventListener('mouseover', colorSquare);
+        square.addEventListener('mousedown', colorSquareClick);
     }
 }
 
 makeBoard(16);
 
-function changeBoardSize(e) {
+function changeBoardSize() {
     if (this.value >= 2 && this.value <= 100)
         makeBoard(this.value);
     else
         console.error("Out of range")
+}
+
+function colorSquare(e) {
+    if (e.buttons === 1){
+        if (color === "random"){
+            this.style.background = `hsl(${Math.random()*360}, 100%, 50%)`;
+        }
+        else {
+            this.style.background = color;
+        }    
+    }
+}
+
+function colorSquareClick() {
+    if (color === "random"){
+        this.style.background = `hsl(${Math.random()*360}, 100%, 50%)`;
+    }
+    else {
+        this.style.background = color;
+    }   
+    
+}
+
+
+function changeColor(choice) {
+    color = choice;
 }
 
 let input = document.querySelector('input');
